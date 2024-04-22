8c4f84e5009fa413b5ac60b167524c7480f99bf7
swadhinbiswas <swadhinbiswas.cse@gmail.com>
2024-04-22 23:46:48 +0600

    update

---
+++ app/bot/helper/ext-utils/help_message.py
@@ -0,0 +1,19 @@
+mirror="""
+<b>🔰 Mirror Bot</b>
+<b>🔰 Version:</b> <code>1.0.0</code>
+/cmd link
+
+<b>By replying to link/file</b>: <code>mirror</code>
+
+/cmd -n new name -e -up upload destination
+<b>Example:</b> <code>/cmd -n new name -e -up upload destination</code>
+
+<b>NOTE:</b>
+1. Commands that start with <b>qb</b> are ONLY for torrents.
+
+"""
+
+yt="""<b>🔰 YouTube Downloader</b>
+
+
+"""
